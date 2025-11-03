cb20ae617ead9df9cc81e68592908e7aae40d9dc
Zhannishk <janiaa.uk@gmail.com>
2025-11-03 17:34:59 +0500

    first commit bismilla

---
+++ Sore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sore
+//
+//  Created by Zhaniya Utemuratova on 03.11.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
