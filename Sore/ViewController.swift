caa82d420c026d934860b79a8f5b021ef3ecab0f
Zhannishk <janiaa.uk@gmail.com>
2025-11-10 16:11:51 +0500

    sign up/sign page

--- Sore/ViewController.swift
+++ Sore/ViewController.swift
@@ -1,19 +1,102 @@
-//
-//  ViewController.swift
-//  Sore
-//
-//  Created by Zhaniya Utemuratova on 03.11.2025.
-//
+import SwiftUI
 
-import UIKit
+struct ContentView: View {
+    @State private var email = ""
+    @State private var password = ""
+    
+    var body: some View {
+        ZStack {
+            Color.black
+            
+            RoundedRectangle(cornerRadius: 30, style: .continuous)
+                .foregroundStyle(.linearGradient(colors: [.black, .blue], startPoint:
+                        .topLeading, endPoint: .bottomTrailing))
+                .frame(width: 1000, height: 400)
+                .rotationEffect(.degrees(135))
+                .offset(y: -350)
+            
+            VStack(spacing:20){
+                Text("Buy. Read. Share. Repeat.")
+                    .foregroundStyle(.white)
+                    .font(.system(size: 15, weight: .bold, design: .rounded))
+                    .fontDesign(.monospaced)
+                    .offset(x: -60, y: -100)
+                
+                TextField("Email", text: $email)
+                    .foregroundStyle(.white)
+                    .textFieldStyle(.plain)
+                    .placeholder(when: email.isEmpty) {
+                        Text("Email")
+                            .foregroundStyle(.white)
+                            .bold()
+                    }
+                
+                Rectangle()
+                    .frame(width: 350, height: 1)
+                    .foregroundStyle(.white)
+                
+                SecureField("Password", text: $password)
+                    .foregroundStyle(.white)
+                    .textFieldStyle(.plain)
+                    .placeholder(when: password.isEmpty) {
+                        Text("Password")
+                            .foregroundStyle(.white)
+                            .bold()
+                    }
+                
+                Rectangle()
+                    .frame(width: 350, height: 1)
+                    .foregroundStyle(.white)
+                
+                Button {
+                    //sign up
+                } label: {
+                    Text("Sign up")
+                        .bold()
+                        .frame(width: 200, height: 40)
+                        .background(
+                            RoundedRectangle(cornerRadius: 10, style: .continuous)
+                                .fill(Color(red: 6/255, green: 42/255, blue: 115/255))
+                        )
+                        .foregroundColor(.white)
+                }
+                .padding(.top)
+                .offset(y: 100)
+                Button {
+                    //login
+                } label: {
+                    Text("Already have an account? Login")
+                        .bold()
+                        .foregroundStyle(.white)
+                }
+                .padding(.top)
+                .offset(y: 110)
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+                
+            }
+            .frame(width: 350)
+        }
+        .ignoresSafeArea()
     }
-
-
 }
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+extension View {
+    func placeholder<Content: View>(
+        when shouldShow: Bool,
+        alignment: Alignment = .leading,
+        @ViewBuilder placeholder: () -> Content) -> some View {
+
+        ZStack(alignment: alignment) {
+            placeholder().opacity(shouldShow ? 1 : 0)
+            self
+        }
+    }
+}
+    
 
